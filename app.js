35a6ab48920dc1f3161675822a04aad38a1e093d
melony <melanie.corletto@sansano.usm.cl>
2019-05-03 10:18:31 -0400

    se elimino?

--- app.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-fs.writeFile("./potito.txt","Bbesitooooo",(err)=> {
-  console.log("Lo de nosotros eh un secreto bbesitooo");
-  if(err) throw err;
-})
-
-console.log("Fin del comunicado");
